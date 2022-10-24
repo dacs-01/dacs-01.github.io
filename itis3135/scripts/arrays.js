079ad924ff9697ed85d284ddc3f5f225392ac9a8
Daniel Cisneros Siliezar <dcisneros0173@gmail.com>
2022-10-24 13:38:06 -0400

    Added arrays file and better nav bar

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,79 +1,73 @@
 var people = [];
 var salaries = [];
-
 var $ = function (id) { return document.getElementById(id); };
 
-window.onload = function () {
-    document.getElementById("display_results").onclick = displayResults;
-    document.getElementById("add_salary").onclick = addSalary;
-    document.getElementById("display_salary").onclick = displaySalary;
-    document.getElementById("names").focus();
+window.onload = function () 
+{
+    $("display_results").onclick = displayResults;
+    $("add_salary").onclick = addSalary;
+    $("display_salary").onclick = displaySalary;
+    $("people").focus();
 };
 
-
-
-function addSalary(){
-    var person = document.getElementById("names").value;
-    var salary = document.getElementById("salary").value;
+function addSalary()
+{
+    var person = $("people").value;
+    var salary = $("salary").value;
     
-   if(salaryInput>150 || salaryInput<10|| salaryInput == ""){
-       alert("You must enter a name and a valid salary");
-       salaryInput = "";
-       $("names").focus();
+   if (salary < 30000 || salary == "" || isNaN(salary)
+   ){
+       alert("You must enter a valid salary");
+       salary = "";
+       $("people").focus();
    }
-   else{
-    salaryInput = parseInt(salaryInput);
-    if(names.includes(nameInput)){
-       var index =  names.indexOf(nameInput);
-       salary[index] = salaryInput;
-
+   else
+   {
+    salary = parseInt(salary);
+    if(people.includes(person))
+    {
+       var index =  people.indexOf(person);
+       salaries[index] = salary;
+       $("people").focus();
     }
-    else{
-        names.push(nameInput);
-        salary.push(salaryInput);
+    else
+    {
+        people.push(person);
+        salaries.push(salary);
     }
-       
-       $("salary").innerHTML = "" ;
-       $("names").focus();
+       $("salaries").innerHTML = "" ;
    }
-
-
 }
 
-function displayResults(){
-    var average = 0;
-    var sum = 0;
-    var highest;
-    var max = 0;
-
-    for(i=0; i<salary.length-1;i++){
-        sum += salary[i];
+function displayResults()
+{
+    var avg = 0, sum = 0, max = 0, highestSal;
+    for(i = 0; i < salaries.length; i++)
+    {
+        sum += salaries[i];
     }
-    average = (sum/salary.length);
+    avg = (sum/salaries.length).toFixed(2);
 
-    for(j=0;j<salary.length;j++){
-        if(salary[j]>max){
-            max =salary[j];
-            highest=j;
+    for(j = 0; j < salaries.length; j++)
+    {
+        if(salaries[j] > max){
+            max = salaries[j];
+            highestSal = j;
         }
     }
 
-
-    $("results").innerHTML = "<h2>Results</h2>" + "<p>Average salary is " + average + " thousand"
-        +"</p>" + "<p>The highest salary was for " +names[highest] + " with a salary of " + max + "</p>" ;
-  
+    $("results").innerHTML = "<h2>Results</h2>" + "<p>Average of the given salaries is $" + avg
+        + "</p>" + "<p>The highest salary of the company is earned by " + people[highestSal] + " with a salary of $" + max + "</p>" ;
 }
 
-function displaySalary(){
-    
-   
+function displaySalary()
+{
     var html = "";
-    html += "<tr><th>Name</th> <th>Salary</th></tr>"
-    for(k=0;k<salary.length;k++){
-        html+="<tr><td>" + names[k] + "</td><td>" + salary[k]+" k</td></tr>";
+    html += "<tr><th>Person</th> <th>Salary (in $USD)</th></tr>"
+    for(k = 0; k < salaries.length; k++)
+    {
+        html+="<tr><td>" + people[k] + "</td><td>" + salaries[k]+"</td></tr>";
     }
     html+="</table>";
     $("results_table").innerHTML = "<h2>Salaries</h2>" + html;
-    
-
 }
